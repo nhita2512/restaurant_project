fccc10ceddc6f752e2a0f598d715d119d7cd5f45
nhita2512 <yennhitale2@gmail.com>
2021-01-25 23:02:34 +0700

    Them file vao

---
+++ main.js
@@ -0,0 +1,96 @@
+// Animated slideshow cho Background img
+// Phai khai bao var slideID o ngoai Ham showAnimatedSlides()
+var slideId = 0;
+showAnimatedSlides();
+
+function showAnimatedSlides() {
+  var i;
+  var Slides = document.getElementsByClassName("animatedSlide");
+  for (i = 0; i < Slides.length; i++) {
+    Slides[i].style.display = "none";
+  }
+  slideId++;
+  if (slideId > Slides.length) {
+    slideId = 1;
+  }
+  Slides[slideId - 1].style.display = "block";
+  setTimeout(showAnimatedSlides, 2000); // Change image every seconds
+}
+
+// Light box of section HinhAnh
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+var slideIndex = 1;
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+// Phan Btn BAckToTop
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// SmoothScroll
+// Modal Contact Form
+// Get the modal
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
